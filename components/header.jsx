5ef794156aababea81b4f53d8618ff73300bf5b8
vaidik jaiswal <vaidikjaiswal@gmail.com>
2025-08-15 14:51:48 +0530

    Update social media links

--- components/header.jsx
+++ components/header.jsx
@@ -13,13 +13,15 @@ export function Header() {
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 max-w-screen-2xl items-center">
         <div className="flex flex-1 items-center justify-end space-x-2">
-          <Link href="https://twitter.com/g" target="_blank" rel="noopener noreferrer">
+          <Link href="https://x.com/vaidikjais" target="_blank" rel="noopener noreferrer">
             <Button variant="ghost" size="icon">
               <Twitter className="h-5 w-5" />
               <span className="sr-only">Twitter</span>
             </Button>
           </Link>
-          <Link href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+          <Link href="https://www.linkedin.com/in/vaidik-jaiswal/
+
+" target="_blank" rel="noopener noreferrer">
             <Button variant="ghost" size="icon">
               <Linkedin className="h-5 w-5" />
               <span className="sr-only">LinkedIn</span>
